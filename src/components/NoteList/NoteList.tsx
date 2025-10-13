afd553aaba617da8f05d2276cece34b72e405794
helen-akateva <akateva17@gmail.com>
2025-10-13 21:33:29 +0300

    update

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,14 +1,22 @@
+import type { Note } from '../../types/note';
 import css from './NoteList.module.css';
 
+interface NoteListProps {
+  notes: Note[];
+   onDelete: (id: string) => void;
+}
 
-<ul className={css.list}>
-	{/* Набір елементів списку нотаток */}
-  <li className={css.listItem}>
-    <h2 className={css.title}>Note title</h2>
-    <p className={css.content}>Note content</p>
-    <div className={css.footer}>
-      <span className={css.tag}>Note tag</span>
-      <button className={css.button}>Delete</button>
-    </div>
-  </li>
-</ul>
+export default function NoteList({ notes, onDelete }: NoteListProps) {
+  return (<ul className={css.list}>
+    { notes.map((note)=> <li key={note.id} className={css.listItem}>
+      <h2 className={css.title}>{note.title }</h2>
+      <p className={css.content} >{ note.content}</p>
+      <div className={css.footer}>
+        <span className={css.tag}>{note.tag }</span>
+        <button className={css.button}  onClick={() => onDelete(note.id)}>Delete</button>
+      </div>
+    </li>)}
+    
+  </ul>);
+}
+
