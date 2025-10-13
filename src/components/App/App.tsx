1125027467f6ed777bfb52e57cc86a743ba09c02
helen-akateva <akareva17@gmail.com>
2025-10-13 19:32:14 +0300

    update

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import css from './App.module.css';
+
+
+<div className={css.app}>
+	<header className={css.toolbar}>
+		{/* Компонент SearchBox */}
+		{/* Пагінація */}
+		{/* Кнопка створення нотатки */}
+  </header>
+</div>
+
