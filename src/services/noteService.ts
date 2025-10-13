be6c0a3c2aaeca2aeffccbb147b948b1dc010863
helen-akateva <akareva17@gmail.com>
2025-10-13 21:33:29 +0300

    update

---
+++ src/services/noteService.ts
@@ -0,0 +1,57 @@
+import axios from "axios";
+import type { Note, NoteTag } from "../types/note";
+
+interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+interface CreateNoteData {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+const BASE_URL = "https://notehub-public.goit.study/api";
+axios.defaults.baseURL = BASE_URL;
+
+export async function fetchNotes(
+  search?: string,
+  tag?: string,
+  page?: number,
+  perPage?: number,
+  sortBy?: string
+): Promise<FetchNotesResponse> {
+  const response = await axios.get<FetchNotesResponse>("/notes", {
+    params: {
+      search,
+      tag,
+      page,
+      perPage,
+      sortBy,
+    },
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+    },
+  });
+  return response.data;
+}
+
+export async function createNote(
+  createNoteData: CreateNoteData
+): Promise<Note> {
+  const res = await axios.post<Note>("/notes", createNoteData, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+    },
+  });
+  return res.data;
+}
+
+export async function deleteNote(id: string): Promise<void> {
+  await axios.delete<Note>(`/notes/${id}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+    },
+  });
+}
