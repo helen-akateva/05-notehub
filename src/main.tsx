8ceab3c1a2cf8bae75032bb93c4b0fac68e4fa7d
helen-akateva <akateva17@gmail.com>
2025-10-13 19:43:57 +0300

    token

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,13 @@
- 
+ import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root") as HTMLDivElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
